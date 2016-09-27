8d10eae48d79c5dfb198013e230b5866f8fff053
MikeDX <mikedx@gmail.com>
2016-09-27 01:55:04 +0100

    Added caveman graphics

--- c/caveman.c
+++ c/caveman.c
@@ -14,20 +14,198 @@ vfd_game game_caveman = {
 	.name = "caveman"
 };
 
-SDL_Surface *gfx[10][15];
+SDL_Surface *gfx[20][20];
 SDL_Surface *bg;
 
-int gfx_x[10][15];
-int gfx_y[10][15];
+int gfx_x[20][20];
+int gfx_y[20][20];
 
 void caveman_setup_gfx(void) {
 	int x = 0;
 	int y = 0;
 	char filename[255];
+	SDL_Rect rect;
 	
 	IMG_Init(IMG_INIT_PNG);
+	memset(gfx_x,0,sizeof(gfx_x));
+	memset(gfx_y,0,sizeof(gfx_y));
+
+	screen = SDL_SetVideoMode(684,199,32,SDL_HWSURFACE);
+
+
+// GFX
+
+	gfx_x[0][0]=27;		gfx_y[0][0]=8;
+	gfx_x[0][1]=53;		gfx_y[0][1]=47;
+	gfx_x[0][2]=50;		gfx_y[0][2]=31;
+	gfx_x[0][3]=68;		gfx_y[0][3]=31;
+	gfx_x[0][4]=55;		gfx_y[0][4]=26;
+	gfx_x[0][5]=50;		gfx_y[0][5]=9;
 	
-	screen = SDL_SetVideoMode(684,193,32,SDL_HWSURFACE);
+	gfx_x[0][6]=19;		gfx_y[0][6]=107; // CAVE
+	
+	gfx_x[0][7]=69;		gfx_y[0][7]=9;
+	gfx_x[0][8]=53;		gfx_y[0][8]=5;
+
+
+	// EGGS
+	gfx_x[0][9]=43;		gfx_y[0][9]=169;
+	gfx_x[0][10]=60;	gfx_y[0][10]=170;
+	gfx_x[0][11]=26;	gfx_y[0][11]=169;
+
+	gfx_x[0][12]=25;	gfx_y[0][12]=150;
+	gfx_x[0][13]=43;	gfx_y[0][13]=149;
+	gfx_x[0][14]=60;	gfx_y[0][14]=149;
+
+	gfx_x[0][15]=33;	gfx_y[0][15]=132;
+	gfx_x[0][16]=53;	gfx_y[0][16]=132;
+
+	// PTERODACTYLS
+	gfx_x[0][17]=17;	gfx_y[0][17]=84;
+	gfx_x[0][18]=20;	gfx_y[0][18]=57;
+
+	// GRID 1
+	gfx_x[1][0]=137;	gfx_y[1][0]=5;
+	gfx_x[1][1]=98;		gfx_y[1][1]=47;
+	gfx_x[1][2]=95;		gfx_y[1][2]=31;
+	gfx_x[1][3]=114;	gfx_y[1][3]=31;
+	gfx_x[1][4]=101;	gfx_y[1][4]=26;
+	gfx_x[1][5]=95;		gfx_y[1][5]=8;
+
+	// TABLE
+	gfx_x[1][6]=96;		gfx_y[1][6]=179;
+
+	gfx_x[1][7]=114;	gfx_y[1][7]=8;
+	gfx_x[1][8]=99;		gfx_y[1][8]=5;
+	
+
+	// BODY 1
+	gfx_x[1][9]=123;	gfx_y[1][9]=138;
+	
+	// EGG
+	gfx_x[1][10]=96;	gfx_y[1][10]=156;
+
+	// MUD?
+	gfx_x[1][11]=115;	gfx_y[1][11]=177;
+
+	gfx_x[1][12]=115;	gfx_y[1][12]=145;
+	gfx_x[1][13]=130;	gfx_y[1][13]=130;
+	gfx_x[1][14]=153;	gfx_y[1][14]=99;
+	gfx_x[1][15]=151;	gfx_y[1][15]=74;
+	gfx_x[1][16]=155;	gfx_y[1][16]=57;
+	gfx_x[1][17]=97;	gfx_y[1][17]=89;
+	gfx_x[1][18]=106;	gfx_y[1][18]=53;
+
+
+	// GRID 2
+
+	// NO 0
+	// MUD
+
+	gfx_x[2][1]=219;	gfx_y[2][1]=6;
+	
+	// AXE
+	gfx_x[2][2]=182; 	gfx_y[2][2]=23;
+	gfx_x[2][3]=218;	gfx_y[2][3]=60;
+	gfx_x[2][4]=182;	gfx_y[2][4]=80;
+	gfx_x[2][5]=213;	gfx_y[2][5]=95;
+
+	// NO 6
+
+	// HEAD UP
+	gfx_x[2][7]=199;	gfx_y[2][7]=112;
+
+	// BODY
+	gfx_x[2][8]=184;		gfx_y[2][8]=139;
+
+	// HEAD DOWN
+	gfx_x[2][9]=183;	gfx_y[2][9]=172;
+
+	// EGG
+	gfx_x[2][10]=182;	gfx_y[2][10]=151;
+
+	// MUD
+	gfx_x[2][11]=220;	gfx_y[2][11]=153;
+
+	// AXE
+	gfx_x[2][12]=182;	gfx_y[2][12]=108;
+
+	// MUD
+	gfx_x[2][13]=198;	gfx_y[2][13]=103;
+	gfx_x[2][14]=200;	gfx_y[2][14]=85;
+	gfx_x[2][15]=192;	gfx_y[2][15]=72;
+	gfx_x[2][16]=182;	gfx_y[2][16]=46;
+
+	// PTERODACTYLS
+	gfx_x[2][17]=182;	gfx_y[2][17]=43;
+	gfx_x[2][18]=190;	gfx_y[2][18]=6;
+
+
+
+
+
+	// GRID 3
+
+	// NO 0
+	// MUD
+
+	gfx_x[3][1]=287;	gfx_y[3][1]=14;
+	
+	// AXE
+	gfx_x[3][2]=249; 	gfx_y[3][2]=27;
+	gfx_x[3][3]=282;	gfx_y[3][3]=61;
+	gfx_x[3][4]=250;	gfx_y[3][4]=77;
+	gfx_x[3][5]=281;	gfx_y[3][5]=94;
+
+	// NO 6
+
+	// HEAD UP
+	gfx_x[3][7]=266;	gfx_y[3][7]=114;
+
+	// BODY
+	gfx_x[3][8]=251;	gfx_y[3][8]=138;
+
+	// HEAD DOWN
+	gfx_x[3][9]=249;	gfx_y[3][9]=169;
+
+	// EGG
+	gfx_x[3][10]=282;	gfx_y[3][10]=159;
+
+	// MUD
+	gfx_x[3][11]=253;	gfx_y[3][11]=148;
+
+	// AXE
+	gfx_x[3][12]=249;	gfx_y[3][12]=108;
+
+	// MUD
+	gfx_x[3][13]=265;	gfx_y[3][13]=106;
+	gfx_x[3][14]=268;	gfx_y[3][14]=82;
+	gfx_x[3][15]=266;	gfx_y[3][15]=65;
+	gfx_x[3][16]=267;	gfx_y[3][16]=28;
+
+	// PTERODACTYLS
+	gfx_x[3][17]=252;	gfx_y[3][17]=35;
+	gfx_x[3][18]=249;	gfx_y[3][18]=6;
+
+
+	for(x=0;x<19;x++) {
+		for(y=0;y<10;y++) {
+			sprintf(filename,"res/gfx/caveman/%d.%d.png",y,x);
+			gfx[y][x]=IMG_Load(filename);		
+			if(gfx[y][x]) {
+				rect.x=gfx_x[y][x];
+				rect.y=gfx_y[y][x];
+				rect.w=gfx[y][x]->w;
+				rect.h=gfx[y][x]->h;
+				SDL_BlitSurface(gfx[y][x],NULL, screen,&rect);
+			}	
+		}
+	}
+
+
+	bg=IMG_Load("res/gfx/caveman/bg3.png");		
+
+	SDL_Flip(screen);
 
 }
 
@@ -35,15 +213,27 @@ void caveman_display_update(void) {
 	int x,y;
 	SDL_Rect rect;
 
+	SDL_BlitSurface(bg, NULL, screen, NULL);
+	for(x=0;x<19;x++) {
+		for(y=0;y<10;y++) {
+			if(gfx[y][x] && (active_game->cpu->display_cache[y]&1<<x)) {
+				rect.x=gfx_x[y][x];
+				rect.y=gfx_y[y][x];
+				rect.w=gfx[y][x]->w;
+				rect.h=gfx[y][x]->h;
+				SDL_BlitSurface(gfx[y][x],NULL, screen,&rect);
+			}	
+
+		}
+	}
 	SDL_Flip(screen);
 	SDL_PauseAudio(0);
 
 }
 void caveman_prepare_display(ucom4cpu *cpu) {
-	uint16_t grid = BITSWAP16(cpu->grid,15,14,13,12,11,10,0,1,2,3,4,5,6,7,8,9);
-	uint16_t plate = BITSWAP16(cpu->plate,15,3,2,6,1,5,4,0,11,10,7,12,14,13,8,9);
-
-	ucom4_display_matrix(cpu, 15, 10, plate, grid);
+	uint8_t grid = BITSWAP8(cpu->grid,0,1,2,3,4,5,6,7);
+	uint32_t plate = BITSWAP24(cpu->plate,23,22,21,20,19,10,11,5,6,7,8,0,9,2,18,17,16,3,15,14,13,12,4,1) | 0x40;
+	ucom4_display_matrix(cpu, 19, 8, plate, grid);
 
 }
 
@@ -54,28 +244,29 @@ void caveman_output_w(ucom4cpu *cpu, int index, uint8_t data)
 
 	int shift;
 
-	if(index == NEC_UCOM4_PORTI)
-		data &=0x7;
+	// if(index == NEC_UCOM4_PORTI)
+	// 	data &=0x7;
 	
 	switch (index) {
+		// C,D: vfd matrix grid
 		case NEC_UCOM4_PORTC:
 		case NEC_UCOM4_PORTD:
-		case NEC_UCOM4_PORTE:
-			// E3: speaker out
-			if (index == NEC_UCOM4_PORTE)
-				level_w(cpu, data >> 3 & 1);
-
-			// C,D,E01: vfd matrix grid
 			shift = (index - NEC_UCOM4_PORTC) * 4;
 			cpu->grid = (cpu->grid & ~(0xf << shift)) | (data << shift);
 			active_game->prepare_display(cpu);
 			break;
 
+		case NEC_UCOM4_PORTE:
 		case NEC_UCOM4_PORTF:
 		case NEC_UCOM4_PORTG:
 		case NEC_UCOM4_PORTH:
 		case NEC_UCOM4_PORTI:
-			shift = (index - NEC_UCOM4_PORTF) * 4;
+			// E3: speaker out
+			if (index == NEC_UCOM4_PORTE)
+				level_w(cpu, data >> 3 & 1);
+
+			// E012,F,G,H,I: vfd matrix plate
+			shift = (index - NEC_UCOM4_PORTE) * 4;
 			cpu->plate = (cpu->plate & ~(0xf << shift)) | (data << shift);
 			active_game->prepare_display(cpu);
 			break;
@@ -95,14 +286,13 @@ uint8_t caveman_input_r(ucom4cpu *cpu, int index)
 	switch (index)
 	{
 		case NEC_UCOM4_PORTA:
-			inp = inputs[2]<<2|inputs[1]<<1|inputs[0];
-
+			inp = inputs[4]<<3|inputs[3]<<3|inputs[2]|inputs[1]<<1|inputs[0]<<2;
 			break;
-		case NEC_UCOM4_PORTB:
-			inp = inputs[4]<<1|inputs[3];
-			break;
+		// case NEC_UCOM4_PORTB:
+		// 	inp = inputs[4]<<1|inputs[3];
+		// 	break;
 	}
-	return inp & 0xf;
+	return inp;
 }
 
 
--- c/ucom4_cpu.h
+++ c/ucom4_cpu.h
@@ -16,8 +16,46 @@
 #define STACK_SIZE 3
 #define BIT(x,n) (((x)>>(n))&1)
 
+#define BITSWAP8(val,B7,B6,B5,B4,B3,B2,B1,B0) \
+                ((BIT(val, B7) <<  7) | \
+                        (BIT(val, B6) <<  6) | \
+                        (BIT(val, B5) <<  5) | \
+                        (BIT(val, B4) <<  4) | \
+                        (BIT(val, B3) <<  3) | \
+                        (BIT(val, B2) <<  2) | \
+                        (BIT(val, B1) <<  1) | \
+                        (BIT(val, B0) <<  0))
+
+
 #define BITSWAP16(val,B15,B14,B13,B12,B11,B10,B9,B8,B7,B6,B5,B4,B3,B2,B1,B0) \
                 ((BIT(val,B15) << 15) | \
+                        (BIT(val,B14) << 14) | \
+                        (BIT(val,B13) << 13) | \
+                        (BIT(val,B12) << 12) | \
+                        (BIT(val,B11) << 11) | \
+                        (BIT(val,B10) << 10) | \
+                        (BIT(val, B9) <<  9) | \
+                        (BIT(val, B8) <<  8) | \
+                        (BIT(val, B7) <<  7) | \
+                        (BIT(val, B6) <<  6) | \
+                        (BIT(val, B5) <<  5) | \
+                        (BIT(val, B4) <<  4) | \
+                        (BIT(val, B3) <<  3) | \
+                        (BIT(val, B2) <<  2) | \
+                        (BIT(val, B1) <<  1) | \
+                        (BIT(val, B0) <<  0))
+
+
+#define BITSWAP24(val,B23,B22,B21,B20,B19,B18,B17,B16,B15,B14,B13,B12,B11,B10,B9,B8,B7,B6,B5,B4,B3,B2,B1,B0) \
+                ((BIT(val,B23) << 23) | \
+                        (BIT(val,B22) << 22) | \
+                        (BIT(val,B21) << 21) | \
+                        (BIT(val,B20) << 20) | \
+                        (BIT(val,B19) << 19) | \
+                        (BIT(val,B18) << 18) | \
+                        (BIT(val,B17) << 17) | \
+                        (BIT(val,B16) << 16) | \
+                		(BIT(val,B15) << 15) | \
                         (BIT(val,B14) << 14) | \
                         (BIT(val,B13) << 13) | \
                         (BIT(val,B12) << 12) | \
@@ -87,8 +125,8 @@ typedef struct _ucom4cpu {
 	int old_icount;
 	uint8_t inp_mux ;
 
-	uint16_t grid;
-	uint16_t plate;
+	uint32_t grid;
+	uint32_t plate;
 
 	int display_wait;                 // led/lamp off-delay in microseconds (default 33ms)
 	int display_maxy;                 // display matrix number of rows
